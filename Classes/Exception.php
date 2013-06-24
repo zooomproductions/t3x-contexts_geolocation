22cd61a1e179c72a1dec50bd90663cd253db494a
Rico Sonntag <rico.sonntag@netresearch.de>
2013-06-24 10:48:09 +0200

    TYPOC-47: Exception class

---
+++ Classes/Exception.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Part of geolocation context extension.
+ *
+ * PHP version 5
+ *
+ * @category   TYPO3-Extensions
+ * @package    Contexts
+ * @subpackage Geolocation
+ * @author     Rico Sonntag <rico.sonntag@netresearch.de>
+ * @license    http://opensource.org/licenses/gpl-license GPLv2 or later
+ * @link       http://github.com/netresearch/contexts_geolocation
+ */
+
+/**
+ * Base exception class.
+ *
+ * @category   TYPO3-Extensions
+ * @package    Contexts
+ * @subpackage Geolocation
+ * @author     Rico Sonntag <rico.sonntag@netresearch.de>
+ * @license    http://opensource.org/licenses/gpl-license GPLv2 or later
+ * @link       http://github.com/netresearch/contexts_geolocation
+ */
+class Tx_Contexts_Geolocation_Exception extends Exception
+{
+}
+?>
